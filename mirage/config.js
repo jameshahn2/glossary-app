d2bd4e6f0e7b7ff006a55b77ac09b05a4536ad7f
James Hahn II <triberocket@Jamess-MacBook-Pro.local>
2019-08-16 14:32:23 -0500

    correct mirage/config.js

--- mirage/config.js
+++ mirage/config.js
@@ -1,10 +1,3 @@
-const convertLinks = function(definition) {
-
-    let cleaned = definition.replace(/<a[^>]* href="([^"]*)"/)
-    let emberLinks = cleaned.replace(/\/en\/Terms\//gi, "/terms/")
-    return emberLinks
-}
-
 export default function() {
   this.namespace = "api";
 this.get("/terms", function({ terms }, { queryParams }) {
@@ -12,11 +5,9 @@ this.get("/terms", function({ terms }, { queryParams }) {
   let results = terms.all();
   if (term) {
     const match = term.toLowerCase();
-    results = results.filter(t => t.term.toLowerCase().includes(match));
+    results = results.filter(t => t.term.toLowerCase().includes( match ));
   }
-  return results.forEach(function(result) {
-    result.definition = convertLinks(result.definition)
-})
+  return results;
 });
-  this.get("terms/:id");
+this.get("terms/:id");
 }
