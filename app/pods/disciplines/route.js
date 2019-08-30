2bf5d61add088c7a49a28ab615a36940e0123597
James Hahn II <triberocket@Jamess-MacBook-Pro.local>
2019-08-30 15:06:39 -0500

    update mirage config

--- app/pods/disciplines/route.js
+++ app/pods/disciplines/route.js
@@ -5,6 +5,6 @@ import Route from "@ember/routing/route";
 export default Route.extend({
   model(params) {
     console.log(params)
-    return this.store.query("term", { disciplines: params.disciplines }).filter(discipline => discipline.disciplineId === this.get("discipline"));;
+    return this.store.query("term", { disciplines: params.disciplines });
   }
 });
--- mirage/config.js
+++ mirage/config.js
@@ -1,7 +1,7 @@
 export default function() {
   this.namespace = "api";
   this.get("/terms", function({ terms }, { queryParams }) {
-    let { term, letter } = queryParams;
+    let { term, letter, disciplines, group } = queryParams;
     let results = terms.all();
     if (term) {
       const match = term.toLowerCase();
@@ -11,13 +11,13 @@ export default function() {
       const match = letter.toLowerCase();
       results = results.filter(t => t.term.toLowerCase()[0] === match);
     }
-    return results;
-  });
-  this.get("/disciplines", function({ terms }, { queryParams }) {
-    let { disciplines } = queryParams;
-    let results = terms.all();
     if (disciplines) {
-      const match = disciplines.toLowerCase();
+      results = results.filter(term => {
+      return term.disciplines.some(discipline => discipline.group === disciplines);
+      });
+    }
+    if (group) {
+      const match = group.toLowerCase();
       results = results.filter(t => t.term.toLowerCase().includes(match));
     }
     return results;
