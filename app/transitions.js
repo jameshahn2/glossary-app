75c2ab80ff55033919cba41843def45111493799
James Hahn II <triberocket@Jamess-MacBook-Pro.local>
2019-09-17 11:02:01 -0500

    install liquid-fire

--- app/transitions.js
+++ app/transitions.js
@@ -1,6 +1,9 @@
-export default function() {
-  this.transition(
-    this.hasClass("ember-modal-dialog"),
-    this.use("crossFade", { duration: 100 })
-  );
+export default function(){
+  // Add your transitions here, like:
+  //   this.transition(
+  //     this.fromRoute('people.index'),
+  //     this.toRoute('people.detail'),
+  //     this.use('toLeft'),
+  //     this.reverse('toRight')
+  //   );
 }
